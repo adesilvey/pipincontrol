5a1f24de6511605378508df1a1b148c6a5b71707
Andrew DeSilvey <Andrew.DeSilvey@charter.com>
2016-08-07 15:05:47 -0700

    Added errorhandling routes, and general error handling. Classed out pipincontrol

--- pipinApi.py
+++ pipinApi.py
@@ -1,25 +1,41 @@
-from flask import Flask
-import pipincontrol
+from flask import Flask, jsonify, request, make_response
+from pipincontrol import PiPinControl as ppc
 
 app = Flask(__name__)
 
-@app.route('/pin/<pin>/on')
-def pin_high(pin):
-    pipincontrol.set_pin_high(pin)
+from werkzeug.debug import DebuggedApplication
+app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
+app.debug = True
 
-    return 'Pin ' + str(pin) + ' set to HIGH'
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({ 'error' : 'Not Found'}), 404)
+
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({ 'error' : 'Bad request'}), 400)
+
+@app.route('/', methods=['GET'])
+def test():
+    return make_response('Everything looks ok here', 200)
+
+@app.route('/pin/<pin>/on', methods=['GET','POST'])
+def pin_high(pin):
+    ppc.set_pin_high(pin)
+
+    return make_response(jsonify({ 'Message' : 'Pin ' + str(pin) + ' set to HIGH' }), 200)
 
 @app.route('/pin/<pin>/off')
 def pin_low(pin):
-    pipincontrol.set_pin_low(pin)
+    ppc.set_pin_low(pin)
 
-    return 'Pin ' + str(pin) + ' set to LOW'
+    return make_response(jsonify({ 'Message' : 'Pin ' + str(pin) + ' set to LOW' }), 200)
 
-@app.route('/pin/<pin>/status')
+@app.route('/pin/<pin>/status', methods=['GET','POST'])
 def pin_status(pin):
-    pipincontrol.get_pin_status(pin)   
+    status = ppc.get_pin_status(pin)   
  
-    return str(status)
+    return make_response(jsonify({ 'Status' : status}), 200)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
--- pipincontrol.py
+++ pipincontrol.py
@@ -1,30 +1,45 @@
 from time import sleep
+import sys
 import subprocess
-
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+class PiPinControl:
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
 
-def set_pin_high(pin):
-    pin = int(pin)
 
-    GPIO.setup(pin, GPIO.OUT)
+    def set_pin_high(self, pin):
+        try:
+            pin = int(pin)
 
-    sleep(0.2)
-    GPIO.output(pin, GPIO.HIGH)
-    sleep(0.2)
+            GPIO.setup(pin, GPIO.OUT)
 
-def set_pin_low(pin):
-    pin = int(pin)
+            sleep(0.2)
+            GPIO.output(pin, GPIO.HIGH)
+            sleep(0.2)
+        except:
+            e = sys.exc_func()[0]
+            print "Exception error: ", e
 
-    GPIO.setup(pin, GPIO.OUT)
+    def set_pin_low(self, pin):
+        try:
+            pin = int(pin)
 
-    sleep(0.2)
-    GPIO.output(pin, GPIO.LOW)
-    sleep(0.2)
+            GPIO.setup(pin, GPIO.OUT)
 
-def get_pin_status(pin):
-    status = subprocess.check_output(['gpio', '-g', 'read', pin])
+            sleep(0.2)
+            GPIO.output(pin, GPIO.LOW)
+            sleep(0.2)
+        except:
+            e = sys.exc_func()[0]
+            print "Exception error: ", e
 
-    return str(status)
+    def get_pin_status(self, pin):
+        try:
+            status = subprocess.check_output(['gpio', '-g', 'read', pin])
+            
+            return str(status)
+        except:
+            e = sys.exc_func()[0]
+            print "Exception error: ", e
